168d7bb0a7c175f2c61fb9e3df61d96b30bf3d5a
Michael Carr <mcarr2468@gmail.com>
2021-11-16 13:34:47 -0600

    V1.8 added underline animation to the navbar links

--- src/App.js
+++ src/App.js
@@ -21,12 +21,14 @@ function App() {
     */}
     <nav className="navbar" id="navbar">
       <ul>
-        <li className="navbar__item"><a href="#welcome-section">Home</a></li>
-        <li className="navbar__item"><a href="#skills">Skills</a> </li>
-        <li className="navbar__item"><a href="#projects">Projects</a></li>
-        <li className="navbar__item"><a href="#contact">Contact</a></li>
+        <li className="navbar__item"><a  className="navbar__link" href="#welcome-section">Home</a></li>
+        <li className="navbar__item"><a  className="navbar__link" href="#skills">Skills</a> </li>
+        <li className="navbar__item"><a  className="navbar__link" href="#projects">Projects</a></li>
+        <li className="navbar__item"><a  className="navbar__link" href="#contact">Contact</a></li>
         <li className="navbar__item">
-          <button className="resume-btn"><a href="/download-resume-link-here">Resume</a></button>
+          <button className="resume-btn">
+            <a href="/download-resume-link-here">Resume</a>
+          </button>
         </li>
       </ul>
     </nav>
